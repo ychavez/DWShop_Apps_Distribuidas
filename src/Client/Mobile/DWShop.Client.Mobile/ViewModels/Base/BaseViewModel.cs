cd11452781c6c069e313cc7e1b778f7b0c03071d
Yael Chavez <you400@gmail.com>
2023-07-03 19:47:12 -0600

    base viewmodel

--- src/Client/Mobile/DWShop.Client.Mobile/ViewModels/Base/BaseViewModel.cs
+++ src/Client/Mobile/DWShop.Client.Mobile/ViewModels/Base/BaseViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DWShop.Client.Mobile.ViewModels.Base
+﻿namespace DWShop.Client.Mobile.ViewModels.Base
 {
-    internal class BaseViewModel
+    public abstract class BaseViewModel: ObservableObject, INavigation
     {
     }
 }
